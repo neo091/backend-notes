1582c5321ffa6f1c4402d82709b2558d5bab44fa
neo091 <marcosdelgadomac@gmail.com>
2024-01-26 18:46:13 +0100

    now fix

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Note = require("./modules/Note")
 const app = express()
 
 const morgan_tiny = morgan("tiny")
+
+
 app.use(morgan_tiny)
 app.use(express.json())
 app.use(cors())
@@ -16,26 +18,38 @@ app.get("/", (req, res) => {
 })
 
 app.get("/api/notes", (req, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+    Note.find({})
+        .then(notes => {
+            response.json(notes)
+        })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     const id = String(request.params.id)
-    response.json({ id: id })
+
+    Note.findById(id).then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
+
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    const id = String(request.params.id)
+    Note.findByIdAndDelete(id).then(result => {
+        console.log('Deleted!')
+        response.status(204).end()
+    }).catch((err) => next(err))
+
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
     if (body.content === undefined) {
-        return response.status(404).json({
+        return response.status(400).json({
             error: 'content missing'
         })
     }
@@ -46,19 +60,30 @@ app.post('/api/notes', (request, response) => {
         date: new Date()
     })
 
-    note.save().then(saved => {
-        console.log('note saved!')
-        response.json(saved)
-    })
-        .catch(error => {
-            console.log('error', error)
+    note.save()
+        .then(saved => saved.toJSON())
+        .then(res => {
+            response.json(res)
         })
-
-
+        .catch((error) => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error("eror: ", error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}  use ctrl+c to close server`)
-    console.log(`Api url: http://localhost:${process.env.PORT}  use ctrl+c to close server`)
+    console.log(`Api Notes url: http://localhost:${process.env.PORT}/api/notes  use ctrl+c to close server`)
 })
